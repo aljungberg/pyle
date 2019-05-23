586a988b274f141b824ac448dcd9fee0da7f444f
Alexander Ljungberg <aljungberg@slevenbits.com>
2019-05-23 09:55:34 +0100

    New: use setuptools, fix minor setup issues.
    No need to include the test module in `py_modules`; add `test_suite` to allow `python setup.py test`, fix formatting.
    
    Refs #2.

--- setup.py
+++ setup.py
@@ -3,18 +3,21 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='pyle',
-      version='0.4',
-      description='Use Python for shell one-liners.',
-      author='Alexander Ljungberg',
-      author_email='aljungberg@slevenbits.com',
-      url='https://github.com/aljungberg/pyle',
-      py_modules=['pyle', 'pyle_test'],
-      scripts=['pyle'],
-      keywords=["shell"],
-      classifiers=[
+setup(
+    name='pyle',
+    version='0.4',
+    description='Use Python for shell one-liners.',
+    author='Alexander Ljungberg',
+    author_email='aljungberg@slevenbits.com',
+    url='https://github.com/aljungberg/pyle',
+    license='BSD',
+    py_modules=['pyle'],
+    scripts=['pyle'],
+    test_suite='pyle_test',
+    keywords=["shell"],
+    classifiers=[
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
         "Development Status :: 3 - Beta",
@@ -25,19 +28,19 @@ setup(name='pyle',
         "Operating System :: POSIX",
         "Topic :: System :: Shells",
         "Topic :: System :: Systems Administration"
-        ],
+    ],
     long_description="""\
-Use Python for shell one-liners
--------------------------------
-
-Pyle makes it easy to use Python as a replacement for command line tools
-such as ``sed`` or ``perl``. 
-
-See https://github.com/aljungberg/pyle for more information.
-""",
-      install_requires=[
+    Use Python for shell one-liners
+    -------------------------------
+    
+    Pyle makes it easy to use Python as a replacement for command line tools
+    such as ``sed`` or ``perl``. 
+    
+    See https://github.com/aljungberg/pyle for more information.
+    """,
+    long_description_content_type='text/markdown',
+    install_requires=[
         'sh >= 1.12.14',
         'future >= 0.17.1',
-      ],
-
-      )
+    ],
+)
